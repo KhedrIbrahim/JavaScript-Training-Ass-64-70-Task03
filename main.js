0f27f7c3c4ae9e0c94d3663ca0eee89766a9ea9c
Khedr Ibrahim <108041341+KhedrIbrahim@users.noreply.github.com>
2023-01-04 08:16:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+/*
+function checker(zName) {
+return function (status) {
+    return function (salary) {
+    return status === "Available" ? `${zName}, My Salary Is ${salary}` : `Iam Not Avaialble`;
+    };
+};
+}
+*/
+
+
+//  Arrow Function ( Currying Function Technique )
+let checker = zName => status => salary => status === "Available" ? `${zName}, My Salary Is ${salary}`: `I am Not Availble`
+
+console.log(checker("Osama")("Available")(4000)); // Osama, My Salary Is 4000
+console.log(checker("Ahmed")("Not Available")()); // Iam Not Avaialble
